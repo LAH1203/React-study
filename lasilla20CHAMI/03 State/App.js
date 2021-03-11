b7379363e7c009c79469b3f312aee00762878aeb
JEONG CHA MI <lasilla20@naver.com>
2021-03-12 02:00:34 +0900

    commit

---
+++ lasilla20CHAMI/03 State/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: {title: 'WEB', sub:'world wide web'},
+      contents: [
+        {id:1, title: 'HTML', desc:'HTML is HyperText...'},
+        {id:2, title: 'CSS', desc:'CSS is for design'},
+        {id:3, title: 'JavaScript', desc:'JavaScript is for interactive'},
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        <TOC data={this.state.contents}></TOC>
+        <Content></Content>
+      </div>
+    );
+  }
+}
+
+export default App;
